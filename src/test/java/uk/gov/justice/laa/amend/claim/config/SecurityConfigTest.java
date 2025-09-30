14daa0ed6efe32e7acb90d3229fb8dc5ddbff5b7
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2025-09-30 09:25:37 +0100

    BC-14 : Added failedOAuthRedirectsToLoginErrorPage test

--- src/test/java/uk/gov/justice/laa/amend/claim/config/SecurityConfigTest.java
+++ src/test/java/uk/gov/justice/laa/amend/claim/config/SecurityConfigTest.java
@@ -1,6 +1,7 @@
 package uk.gov.justice.laa.amend.claim.config;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,7 +13,12 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,7 +29,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -37,7 +45,7 @@ public class SecurityConfigTest {
     private MockMvc mockMvc;
 
     @TestConfiguration
-    static class MockSecurityBeans {
+    static class MockSecurityConfigBeans {
 
         @Bean
         ClientRegistrationRepository clientRegistrationRepository() {
@@ -88,31 +96,68 @@ public class SecurityConfigTest {
             .andExpect(redirectedUrl("/not-authorised"));
     }
 
-    @Test
-    void mapRolesToAuthoritiesWhenRolesAreCommaSeparated() {
-        SecurityConfig config = new SecurityConfig();
-        Map<String, Object> attributes = Map.of("LAA_APP_ROLES", "USER,ADMIN");
+    @Nested
+    @Import(MockSecurityConfigBeans.class)
+    class SecurityConfigGetAuthoritiesTest {
 
-        Set<GrantedAuthority> result = config.getAuthorities(attributes);
+        @Test
+        void mapRolesToAuthoritiesWhenRolesAreCommaSeparated() {
+            SecurityConfig config = new SecurityConfig();
+            Map<String, Object> attributes = Map.of("LAA_APP_ROLES", "USER,ADMIN");
 
-        Assertions.assertTrue(result.stream()
-            .anyMatch(x -> x.getAuthority().equals("ROLE_USER")));
+            Set<GrantedAuthority> result = config.getAuthorities(attributes);
 
-        Assertions.assertTrue(result.stream()
-            .anyMatch(x -> x.getAuthority().equals("ROLE_ADMIN")));
+            Assertions.assertTrue(result.stream()
+                .anyMatch(x -> x.getAuthority().equals("ROLE_USER")));
+
+            Assertions.assertTrue(result.stream()
+                .anyMatch(x -> x.getAuthority().equals("ROLE_ADMIN")));
+        }
+
+        @Test
+        void mapRolesToAuthoritiesWhenRolesAreInAList() {
+            SecurityConfig config = new SecurityConfig();
+            Map<String, Object> attributes = Map.of("LAA_APP_ROLES", List.of("USER", "ADMIN"));
+
+            Set<GrantedAuthority> result = config.getAuthorities(attributes);
+
+            Assertions.assertTrue(result.stream()
+                .anyMatch(x -> x.getAuthority().equals("ROLE_USER")));
+
+            Assertions.assertTrue(result.stream()
+                .anyMatch(x -> x.getAuthority().equals("ROLE_ADMIN")));
+        }
     }
 
-    @Test
-    void mapRolesToAuthoritiesWhenRolesAreInAList() {
-        SecurityConfig config = new SecurityConfig();
-        Map<String, Object> attributes = Map.of("LAA_APP_ROLES", List.of("USER", "ADMIN"));
+    @Nested
+    @Import(SecurityConfigOidcUserServiceTest.MockSecurityConfigOidcUserServiceBeans.class)
+    class SecurityConfigOidcUserServiceTest {
 
-        Set<GrantedAuthority> result = config.getAuthorities(attributes);
+        @Autowired
+        private OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService;
 
-        Assertions.assertTrue(result.stream()
-            .anyMatch(x -> x.getAuthority().equals("ROLE_USER")));
+        @TestConfiguration
+        static class MockSecurityConfigOidcUserServiceBeans {
 
-        Assertions.assertTrue(result.stream()
-            .anyMatch(x -> x.getAuthority().equals("ROLE_ADMIN")));
+            @Bean
+            ClientRegistrationRepository clientRegistrationRepository() {
+                return mock(ClientRegistrationRepository.class);
+            }
+
+            @Bean
+            OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService() {
+                return mock(OAuth2UserService.class);
+            }
+        }
+
+        @Test
+        void failedOAuthRedirectsToLoginErrorPage() throws Exception {
+            when(oAuth2UserService.loadUser(any()))
+                .thenThrow(new OAuth2AuthenticationException(new OAuth2Error("invalid_token")));
+
+            mockMvc.perform(get("/login/oauth2/code/google"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login?error"));
+        }
     }
 }
