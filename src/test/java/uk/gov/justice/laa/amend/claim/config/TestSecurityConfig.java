33a2963c16ab6dde1464623a990e55e1467ec915
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2025-09-30 20:39:58 +0100

    Added a TestSecurityConfig.

---
+++ src/test/java/uk/gov/justice/laa/amend/claim/config/TestSecurityConfig.java
@@ -0,0 +1,21 @@
+package uk.gov.justice.laa.amend.claim.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Profile("test")
+@Configuration
+@EnableWebSecurity
+public class TestSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChainTest(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
--- src/test/java/uk/gov/justice/laa/amend/claim/controllers/HomePageControllerTest.java
+++ src/test/java/uk/gov/justice/laa/amend/claim/controllers/HomePageControllerTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.justice.laa.amend.claim.config.TestSecurityConfig;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -13,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(HomePageController.class)
 @AutoConfigureMockMvc(addFilters = false)
+@Import(TestSecurityConfig.class)
 public class HomePageControllerTest {
 
     @Autowired
