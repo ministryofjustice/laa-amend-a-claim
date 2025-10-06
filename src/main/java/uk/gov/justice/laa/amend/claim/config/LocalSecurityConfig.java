7c350e69c26abc550304038abe0b7e0d2a33ea3a
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2025-10-06 10:09:23 +0100

    Enabling csrf. Adding token explicitly to form html

--- src/main/java/uk/gov/justice/laa/amend/claim/config/LocalSecurityConfig.java
+++ src/main/java/uk/gov/justice/laa/amend/claim/config/LocalSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Profile("local")
@@ -15,8 +14,7 @@ public class LocalSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChainLocal(final HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-            .csrf(AbstractHttpConfigurer::disable);
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
 }
