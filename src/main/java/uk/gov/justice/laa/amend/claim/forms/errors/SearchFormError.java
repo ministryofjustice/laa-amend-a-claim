abc7bc93822d5c04f7f2ea0ab91c2a6876dc536f
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2025-10-09 07:44:27 +0100

    BC-137: Refactoring FormError.

--- src/main/java/uk/gov/justice/laa/amend/claim/forms/errors/SearchFormError.java
+++ src/main/java/uk/gov/justice/laa/amend/claim/forms/errors/SearchFormError.java
@@ -2,7 +2,9 @@ package uk.gov.justice.laa.amend.claim.forms.errors;
 
 import org.thymeleaf.spring6.util.DetailedError;
 
-public class SearchFormError extends FormError<SearchFormError> {
+import java.util.Map;
+
+public class SearchFormError extends FormError {
 
     public SearchFormError(DetailedError error) {
         super(error);
@@ -13,24 +15,12 @@ public class SearchFormError extends FormError<SearchFormError> {
     }
 
     @Override
-    public int compareTo(SearchFormError other) {
-        if (other == null) return 1;
-
-        int thisOrder = getFieldOrder(this.getFieldName());
-        int otherOrder = getFieldOrder(other.getFieldName());
-
-        return Integer.compare(thisOrder, otherOrder);
-    }
-
-    private int getFieldOrder(String fieldName) {
-        if (fieldName == null) return Integer.MAX_VALUE;
-
-        return switch (fieldName) {
-            case "providerAccountNumber" -> 1;
-            case "submissionDateMonth" -> 2;
-            case "submissionDateYear" -> 3;
-            case "referenceNumber" -> 4;
-            default -> Integer.MAX_VALUE;
-        };
+    protected Map<String, Integer> getFieldOrderMap() {
+        return Map.of(
+            "providerAccountNumber", 1,
+            "submissionDateMonth",  2,
+            "submissionDateYear",   3,
+            "referenceNumber",      4
+        );
     }
 }
