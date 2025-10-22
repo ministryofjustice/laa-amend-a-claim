bc69a8ad2c7a14883eea658647ebb9aaf2c1f04a
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2025-10-22 15:48:05 +0100

    Added run script

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# PROFILE: `local` (default) or `dev`
+PROFILE=${1:-local}
+
+export $(grep -v '^#' .env | xargs)
+
+if [ "$PROFILE" = "local" ]; then
+  docker-compose up -d
+  export CLAIMS_API="http://localhost:8081"
+else
+  export CLAIMS_API="https://main-laa-data-claims-api-uat.cloud-platform.service.justice.gov.uk/"
+fi
+
+./gradlew bootRun --args="--spring.profiles.active=${PROFILE}"
