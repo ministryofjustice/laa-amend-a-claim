b6c9114c3e322e24c066d3e00623ae3351bba03f
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2025-11-11 17:18:33 +0000

    BC-243: Using @ParameterizedTest

--- src/test/java/uk/gov/justice/laa/amend/claim/controllers/ChangeMonetaryValueControllerTest.java
+++ src/test/java/uk/gov/justice/laa/amend/claim/controllers/ChangeMonetaryValueControllerTest.java
@@ -1,9 +1,8 @@
 package uk.gov.justice.laa.amend.claim.controllers;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
@@ -15,344 +14,127 @@ import uk.gov.justice.laa.amend.claim.config.ThymeleafConfig;
 import uk.gov.justice.laa.amend.claim.models.Assessment;
 
 import java.math.BigDecimal;
+import java.util.Map;
 import java.util.UUID;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ActiveProfiles("local")
 @WebMvcTest(ChangeMonetaryValueController.class)
 @Import({LocalSecurityConfig.class, ThymeleafConfig.class})
-public class ChangeMonetaryValueControllerTest {
-
-    private String submissionId;
-    private String claimId;
-    private String path;
-    private MockHttpSession session;
-    private String redirectUrl;
-    private String prefix;
+class ChangeMonetaryValueControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    private String submissionId;
+    private String claimId;
+    private MockHttpSession session;
+    private String redirectUrl;
+
+    private static final Map<String, FieldAccessors> COST_MAPPINGS = Map.ofEntries(
+        Map.entry("profit-costs", new FieldAccessors(Assessment::getNetProfitCostsAmount, Assessment::setNetProfitCostsAmount)),
+        Map.entry("disbursements", new FieldAccessors(Assessment::getDisbursementAmount, Assessment::setDisbursementAmount)),
+        Map.entry("disbursements-vat", new FieldAccessors(Assessment::getDisbursementVatAmount, Assessment::setDisbursementVatAmount)),
+        Map.entry("counsel-costs", new FieldAccessors(Assessment::getNetCostOfCounselAmount, Assessment::setNetCostOfCounselAmount)),
+        Map.entry("detention-travel-and-waiting-costs", new FieldAccessors(Assessment::getTravelAndWaitingCostsAmount, Assessment::setTravelAndWaitingCostsAmount)),
+        Map.entry("jr-form-filling-costs", new FieldAccessors(Assessment::getJrFormFillingAmount, Assessment::setJrFormFillingAmount)),
+        Map.entry("travel-costs", new FieldAccessors(Assessment::getNetTravelCostsAmount, Assessment::setNetTravelCostsAmount)),
+        Map.entry("waiting-costs", new FieldAccessors(Assessment::getNetWaitingCostsAmount, Assessment::setNetWaitingCostsAmount))
+    );
+
     @BeforeEach
-    public void setup() {
+    void setup() {
         submissionId = UUID.randomUUID().toString();
         claimId = UUID.randomUUID().toString();
         session = new MockHttpSession();
         redirectUrl = String.format("/submissions/%s/claims/%s", submissionId, claimId);
     }
 
-    @Nested
-    class ChangeProfitCostsTests {
-
-        @BeforeEach
-        public void setup() {
-            prefix = "profit-costs";
-            path = String.format("/submissions/%s/claims/%s/%s", submissionId, claimId, prefix);
-        }
-
-        @Test
-        public void testGetReturnsView() throws Exception {
-            getReturnsView();
-        }
-
-        @Test
-        public void testGetReturnsViewWhenQuestionAlreadyAnswered() throws Exception {
-            getReturnsViewWhenQuestionAlreadyAnswered(Assessment::setNetProfitCostsAmount);
-        }
-
-        @Test
-        public void testPostSavesValueAndRedirects() throws Exception {
-            postSavesValueAndRedirects(Assessment::getNetProfitCostsAmount);
-        }
-
-        @Test
-        public void testPostReturnsBadRequestForInvalidValue() throws Exception {
-            postReturnsBadRequestForInvalidValue();
-        }
+    private static Stream<String> validCosts() {
+        return COST_MAPPINGS.keySet().stream();
     }
 
-    @Nested
-    class ChangeDisbursementsTests {
-
-        @BeforeEach
-        public void setup() {
-            prefix = "disbursements";
-            path = String.format("/submissions/%s/claims/%s/%s", submissionId, claimId, prefix);
-        }
-
-        @Test
-        public void testGetReturnsView() throws Exception {
-            getReturnsView();
-        }
-
-        @Test
-        public void testGetReturnsViewWhenQuestionAlreadyAnswered() throws Exception {
-            getReturnsViewWhenQuestionAlreadyAnswered(Assessment::setDisbursementAmount);
-        }
-
-        @Test
-        public void testPostSavesValueAndRedirects() throws Exception {
-            postSavesValueAndRedirects(Assessment::getDisbursementAmount);
-        }
-
-        @Test
-        public void testPostReturnsBadRequestForInvalidValue() throws Exception {
-            postReturnsBadRequestForInvalidValue();
-        }
-    }
-
-    @Nested
-    class ChangeDisbursementsVatTests {
-
-        @BeforeEach
-        public void setup() {
-            prefix = "disbursements-vat";
-            path = String.format("/submissions/%s/claims/%s/%s", submissionId, claimId, prefix);
-        }
-
-        @Test
-        public void testGetReturnsView() throws Exception {
-            getReturnsView();
-        }
-
-        @Test
-        public void testGetReturnsViewWhenQuestionAlreadyAnswered() throws Exception {
-            getReturnsViewWhenQuestionAlreadyAnswered(Assessment::setDisbursementVatAmount);
-        }
-
-        @Test
-        public void testPostSavesValueAndRedirects() throws Exception {
-            postSavesValueAndRedirects(Assessment::getDisbursementVatAmount);
-        }
-
-        @Test
-        public void testPostReturnsBadRequestForInvalidValue() throws Exception {
-            postReturnsBadRequestForInvalidValue();
-        }
-    }
-
-    @Nested
-    class ChangeCounselCostsTests {
-
-        @BeforeEach
-        public void setup() {
-            prefix = "counsel-costs";
-            path = String.format("/submissions/%s/claims/%s/%s", submissionId, claimId, prefix);
-        }
-
-        @Test
-        public void testGetReturnsView() throws Exception {
-            getReturnsView();
-        }
-
-        @Test
-        public void testGetReturnsViewWhenQuestionAlreadyAnswered() throws Exception {
-            getReturnsViewWhenQuestionAlreadyAnswered(Assessment::setNetCostOfCounselAmount);
-        }
-
-        @Test
-        public void testPostSavesValueAndRedirects() throws Exception {
-            postSavesValueAndRedirects(Assessment::getNetCostOfCounselAmount);
-        }
-
-        @Test
-        public void testPostReturnsBadRequestForInvalidValue() throws Exception {
-            postReturnsBadRequestForInvalidValue();
-        }
-    }
-
-    @Nested
-    class ChangeDetentionTravelAndWaitingCostsTests {
-
-        @BeforeEach
-        public void setup() {
-            prefix = "detention-travel-and-waiting-costs";
-            path = String.format("/submissions/%s/claims/%s/%s", submissionId, claimId, prefix);
-        }
-
-        @Test
-        public void testGetReturnsView() throws Exception {
-            getReturnsView();
-        }
-
-        @Test
-        public void testGetReturnsViewWhenQuestionAlreadyAnswered() throws Exception {
-            getReturnsViewWhenQuestionAlreadyAnswered(Assessment::setTravelAndWaitingCostsAmount);
-        }
-
-        @Test
-        public void testPostSavesValueAndRedirects() throws Exception {
-            postSavesValueAndRedirects(Assessment::getTravelAndWaitingCostsAmount);
-        }
-
-        @Test
-        public void testPostReturnsBadRequestForInvalidValue() throws Exception {
-            postReturnsBadRequestForInvalidValue();
-        }
-    }
-
-    @Nested
-    class ChangeJrFormFillingCostsTests {
-
-        @BeforeEach
-        public void setup() {
-            prefix = "jr-form-filling-costs";
-            path = String.format("/submissions/%s/claims/%s/%s", submissionId, claimId, prefix);
-        }
-
-        @Test
-        public void testGetReturnsView() throws Exception {
-            getReturnsView();
-        }
-
-        @Test
-        public void testGetReturnsViewWhenQuestionAlreadyAnswered() throws Exception {
-            getReturnsViewWhenQuestionAlreadyAnswered(Assessment::setJrFormFillingAmount);
-        }
-
-        @Test
-        public void testPostSavesValueAndRedirects() throws Exception {
-            postSavesValueAndRedirects(Assessment::getJrFormFillingAmount);
-        }
-
-        @Test
-        public void testPostReturnsBadRequestForInvalidValue() throws Exception {
-            postReturnsBadRequestForInvalidValue();
-        }
-    }
-
-    @Nested
-    class ChangeTravelCostsTests {
-
-        @BeforeEach
-        public void setup() {
-            prefix = "travel-costs";
-            path = String.format("/submissions/%s/claims/%s/%s", submissionId, claimId, prefix);
-        }
-
-        @Test
-        public void testGetReturnsView() throws Exception {
-            getReturnsView();
-        }
-
-        @Test
-        public void testGetReturnsViewWhenQuestionAlreadyAnswered() throws Exception {
-            getReturnsViewWhenQuestionAlreadyAnswered(Assessment::setNetTravelCostsAmount);
-        }
-
-        @Test
-        public void testPostSavesValueAndRedirects() throws Exception {
-            postSavesValueAndRedirects(Assessment::getNetTravelCostsAmount);
-        }
-
-        @Test
-        public void testPostReturnsBadRequestForInvalidValue() throws Exception {
-            postReturnsBadRequestForInvalidValue();
-        }
-    }
-
-    @Nested
-    class ChangeWaitingCostsTests {
-
-        @BeforeEach
-        public void setup() {
-            prefix = "waiting-costs";
-            path = String.format("/submissions/%s/claims/%s/%s", submissionId, claimId, prefix);
-        }
-
-        @Test
-        public void testGetReturnsView() throws Exception {
-            getReturnsView();
-        }
-
-        @Test
-        public void testGetReturnsViewWhenQuestionAlreadyAnswered() throws Exception {
-            getReturnsViewWhenQuestionAlreadyAnswered(Assessment::setNetWaitingCostsAmount);
-        }
-
-        @Test
-        public void testPostSavesValueAndRedirects() throws Exception {
-            postSavesValueAndRedirects(Assessment::getNetWaitingCostsAmount);
-        }
-
-        @Test
-        public void testPostReturnsBadRequestForInvalidValue() throws Exception {
-            postReturnsBadRequestForInvalidValue();
-        }
-    }
-
-    @Nested
-    class PageNotFoundTests {
-
-        @BeforeEach
-        public void setup() {
-            prefix = "foo";
-            path = String.format("/submissions/%s/claims/%s/%s", submissionId, claimId, prefix);
-        }
-
-        @Test
-        public void testPostReturnsBadRequestForInvalidValue() throws Exception {
-            mockMvc.perform(post(path)
-                    .session(session)
-                    .with(csrf())
-                    .param("value", "1")
-                )
-                .andExpect(status().isNotFound());
-        }
-    }
-
-    private void getReturnsView() throws Exception {
-        mockMvc.perform(get(path))
+    @ParameterizedTest
+    @MethodSource("validCosts")
+    void testGetReturnsView(String cost) throws Exception {
+        mockMvc.perform(get(buildPath(cost)))
             .andExpect(status().isOk())
             .andExpect(view().name("change-monetary-value"))
-            .andExpect(model().attribute("prefix", equalTo(prefix)))
+            .andExpect(model().attribute("prefix", equalTo(cost)))
             .andExpect(model().attribute("form", hasProperty("value", nullValue())));
     }
 
-    private void getReturnsViewWhenQuestionAlreadyAnswered(BiConsumer<Assessment, BigDecimal> f) throws Exception {
+    @ParameterizedTest
+    @MethodSource("validCosts")
+    void testGetReturnsViewWhenQuestionAlreadyAnswered(String cost) throws Exception {
+        FieldAccessors accessors = COST_MAPPINGS.get(cost);
         Assessment assessment = new Assessment();
-        f.accept(assessment, BigDecimal.valueOf(100));
-        session.setAttribute(String.format("%s:assessment", claimId), assessment);
+        accessors.setter.accept(assessment, BigDecimal.valueOf(100));
+        session.setAttribute(claimKey(), assessment);
 
-        mockMvc.perform(get(path).session(session))
+        mockMvc.perform(get(buildPath(cost)).session(session))
             .andExpect(status().isOk())
             .andExpect(view().name("change-monetary-value"))
-            .andExpect(model().attribute("prefix", equalTo(prefix)))
+            .andExpect(model().attribute("prefix", equalTo(cost)))
             .andExpect(model().attribute("form", hasProperty("value", is("100.00"))));
     }
 
-    private void postSavesValueAndRedirects(Function<Assessment, BigDecimal> f) throws Exception {
-        session.setAttribute(String.format("%s:assessment", claimId), new Assessment());
+    @ParameterizedTest
+    @MethodSource("validCosts")
+    void testPostSavesValueAndRedirects(String cost) throws Exception {
+        session.setAttribute(claimKey(), new Assessment());
 
-        mockMvc.perform(post(path)
+        mockMvc.perform(post(buildPath(cost))
                 .session(session)
                 .with(csrf())
-                .param("value", "100")
-            )
+                .param("value", "100"))
             .andExpect(status().is3xxRedirection())
             .andExpect(redirectedUrl(redirectUrl));
 
-        Assessment assessment = (Assessment) session.getAttribute(String.format("%s:assessment", claimId));
+        Assessment updated = (Assessment) session.getAttribute(claimKey());
+        FieldAccessors accessors = COST_MAPPINGS.get(cost);
 
-        Assertions.assertNotNull(assessment);
-        Assertions.assertEquals(new BigDecimal("100.00"), f.apply(assessment));
+        Assertions.assertNotNull(updated);
+        Assertions.assertEquals(new BigDecimal("100.00"), accessors.getter.apply(updated));
     }
 
-    private void postReturnsBadRequestForInvalidValue() throws Exception {
-        mockMvc.perform(
-                post(path)
-                    .session(session)
-                    .with(csrf())
-                    .param("value", "-1")
-            )
+    @ParameterizedTest
+    @MethodSource("validCosts")
+    void testPostReturnsBadRequestForInvalidValue(String cost) throws Exception {
+        mockMvc.perform(post(buildPath(cost))
+                .session(session)
+                .with(csrf())
+                .param("value", "-1"))
             .andExpect(status().isBadRequest())
             .andExpect(view().name("change-monetary-value"));
     }
+
+    @Test
+    void testPageNotFound() throws Exception {
+        mockMvc.perform(post(buildPath("foo"))
+                .session(session)
+                .with(csrf())
+                .param("value", "1"))
+            .andExpect(status().isNotFound());
+    }
+
+    private String buildPath(String cost) {
+        return String.format("/submissions/%s/claims/%s/%s", submissionId, claimId, cost);
+    }
+
+    private String claimKey() {
+        return String.format("%s:assessment", claimId);
+    }
+
+    private record FieldAccessors(
+        Function<Assessment, BigDecimal> getter,
+        BiConsumer<Assessment, BigDecimal> setter
+    ) {}
 }
