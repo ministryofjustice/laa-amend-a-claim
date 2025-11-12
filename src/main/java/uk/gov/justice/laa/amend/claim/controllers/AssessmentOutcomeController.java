ffd9d6eb055071d2c2c30bf2aede00689eaff655
spencer.howe <spencer.howe@justice.gov.uk>
2025-11-12 18:34:40 +0000

    fix: amend checkstyle

--- src/main/java/uk/gov/justice/laa/amend/claim/controllers/AssessmentOutcomeController.java
+++ src/main/java/uk/gov/justice/laa/amend/claim/controllers/AssessmentOutcomeController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import uk.gov.justice.laa.amend.claim.forms.AssessmentOutcomeForm;
