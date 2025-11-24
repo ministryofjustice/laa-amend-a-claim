d493994115095c3daeb016d442047ad431fb1b82
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2025-11-24 18:23:21 +0000

    BC-272: Updated AssessmentMapper.

--- src/main/java/uk/gov/justice/laa/amend/claim/mappers/AssessmentMapper.java
+++ src/main/java/uk/gov/justice/laa/amend/claim/mappers/AssessmentMapper.java
@@ -41,17 +41,17 @@ public interface AssessmentMapper {
     @Mapping(target = "netCostOfCounselAmount", expression = "java(mapNetCostOfCounselAmount(claim))")
     @Mapping(target = "travelWaitingCostsAmount", ignore = true)
     @Mapping(target = "travelAndWaitingCostsAmount", ignore = true)
-    @Mapping(target = "adjournedHearingFeeAmount", ignore = true)
-    @Mapping(target = "jrFormFillingAmount", ignore = true)
-    @Mapping(target = "cmrhOralCount", ignore = true)
-    @Mapping(target = "cmrhTelephoneCount", ignore = true)
+    @Mapping(target = "adjournedHearingFeeAmount", expression = "java(mapAdjournedHearingFeeAmount(claim))")
+    @Mapping(target = "jrFormFillingAmount", expression = "java(mapJrFormFillingAmount(claim))")
+    @Mapping(target = "cmrhOralCount", expression = "java(mapCmrhOralCount(claim))")
+    @Mapping(target = "cmrhTelephoneCount", expression = "java(mapCmrhTelephoneCount(claim))")
     @Mapping(target = "isSubstantiveHearing", ignore = true)
-    @Mapping(target = "hoInterview", ignore = true)
+    @Mapping(target = "hoInterview", expression = "java(mapHoInterview(claim))")
     AssessmentPost mapCivilClaimToAssessment(CivilClaimDetails claim, @Context String userId);
 
     @InheritConfiguration(name = "mapClaimToAssessment")
-    @Mapping(target = "netTravelCostsAmount", ignore = true)
-    @Mapping(target = "netWaitingCostsAmount", ignore = true)
+    @Mapping(target = "netTravelCostsAmount", expression = "java(mapNetTravelCostsAmount(claim))")
+    @Mapping(target = "netWaitingCostsAmount", expression = "java(mapNetWaitingCostsAmount(claim))")
     AssessmentPost mapCrimeClaimToAssessment(CrimeClaimDetails claim, @Context String userId);
 
     default AssessmentOutcome mapAssessmentOutcome(ClaimDetails claim) {
@@ -86,8 +86,43 @@ public interface AssessmentMapper {
         return claim != null ? mapToBigDecimal(claim.getCounselsCost()) : null;
     }
 
+    default Integer mapAdjournedHearingFeeAmount(CivilClaimDetails claim) {
+        return claim != null ? mapToInteger(claim.getAdjournedHearing()) : null;
+    }
+
+    default BigDecimal mapJrFormFillingAmount(CivilClaimDetails claim) {
+        return claim != null ? mapToBigDecimal(claim.getJrFormFillingCost()) : null;
+    }
+
+    default Integer mapCmrhOralCount(CivilClaimDetails claim) {
+        return claim != null ? mapToInteger(claim.getCmrhOral()) : null;
+    }
+
+    default Integer mapCmrhTelephoneCount(CivilClaimDetails claim) {
+        return claim != null ? mapToInteger(claim.getCmrhTelephone()) : null;
+    }
+
+    default Integer mapHoInterview(CivilClaimDetails claim) {
+        return claim != null ? mapToInteger(claim.getHoInterview()) : null;
+    }
+
+    default BigDecimal mapNetTravelCostsAmount(CrimeClaimDetails claim) {
+        return claim != null ? mapToBigDecimal(claim.getTravelCosts()) : null;
+    }
+
+    default BigDecimal mapNetWaitingCostsAmount(CrimeClaimDetails claim) {
+        return claim != null ? mapToBigDecimal(claim.getWaitingCosts()) : null;
+    }
+
     private BigDecimal mapToBigDecimal(ClaimField field) {
         if (field != null && field.getAmended() instanceof BigDecimal value) {
+            return value;
+        }
+        return null;
+    }
+
+    private Integer mapToInteger(ClaimField field) {
+        if (field != null && field.getAmended() instanceof Integer value) {
             return value;
         }
         return null;
--- src/test/java/uk/gov/justice/laa/amend/claim/mappers/AssessmentMapperTest.java
+++ src/test/java/uk/gov/justice/laa/amend/claim/mappers/AssessmentMapperTest.java
@@ -6,6 +6,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import uk.gov.justice.laa.amend.claim.models.CivilClaimDetails;
 import uk.gov.justice.laa.amend.claim.models.ClaimField;
+import uk.gov.justice.laa.amend.claim.models.CrimeClaimDetails;
 import uk.gov.justice.laa.amend.claim.models.OutcomeType;
 import uk.gov.justice.laa.dstew.payments.claimsdata.model.AssessmentOutcome;
 import uk.gov.justice.laa.dstew.payments.claimsdata.model.AssessmentPost;
@@ -37,6 +38,11 @@ class AssessmentMapperTest {
         claim.setNetDisbursementAmount(createClaimField(BigDecimal.valueOf(3)));
         claim.setDisbursementVatAmount(createClaimField(BigDecimal.valueOf(4)));
         claim.setCounselsCost(createClaimField(BigDecimal.valueOf(5)));
+        claim.setAdjournedHearing(createClaimField(6));
+        claim.setJrFormFillingCost(createClaimField(BigDecimal.valueOf(7)));
+        claim.setCmrhOral(createClaimField(8));
+        claim.setCmrhTelephone(createClaimField(9));
+        claim.setHoInterview(createClaimField(10));
         claim.setVatApplicable(true);
 
         AssessmentPost assessment = mapper.mapCivilClaimToAssessment(claim, userId);
@@ -49,6 +55,44 @@ class AssessmentMapperTest {
         assertEquals(BigDecimal.valueOf(3), assessment.getDisbursementAmount());
         assertEquals(BigDecimal.valueOf(4), assessment.getDisbursementVatAmount());
         assertEquals(BigDecimal.valueOf(5), assessment.getNetCostOfCounselAmount());
+        assertEquals(6, assessment.getAdjournedHearingFeeAmount());
+        assertEquals(BigDecimal.valueOf(7), assessment.getJrFormFillingAmount());
+        assertEquals(8, assessment.getCmrhOralCount());
+        assertEquals(9, assessment.getCmrhTelephoneCount());
+        assertEquals(10, assessment.getHoInterview());
+        assertEquals(true, assessment.getIsVatApplicable());
+        assertEquals(userId, assessment.getCreatedByUserId());
+    }
+
+    @Test
+    void testMapCrimeClaimToAssessment() {
+        UUID claimId = UUID.randomUUID();
+        UUID claimSummaryFeeId = UUID.randomUUID();
+        String userId = UUID.randomUUID().toString();
+
+        CrimeClaimDetails claim = new CrimeClaimDetails();
+        claim.setClaimId(claimId.toString());
+        claim.setClaimSummaryFeeId(claimSummaryFeeId.toString());
+        claim.setAssessmentOutcome(OutcomeType.NILLED);
+        claim.setFixedFee(createClaimField(BigDecimal.valueOf(1)));
+        claim.setNetProfitCost(createClaimField(BigDecimal.valueOf(2)));
+        claim.setNetDisbursementAmount(createClaimField(BigDecimal.valueOf(3)));
+        claim.setDisbursementVatAmount(createClaimField(BigDecimal.valueOf(4)));
+        claim.setTravelCosts(createClaimField(BigDecimal.valueOf(5)));
+        claim.setWaitingCosts(createClaimField(BigDecimal.valueOf(6)));
+        claim.setVatApplicable(true);
+
+        AssessmentPost assessment = mapper.mapCrimeClaimToAssessment(claim, userId);
+
+        assertEquals(claimId, assessment.getClaimId());
+        assertEquals(claimSummaryFeeId, assessment.getClaimSummaryFeeId());
+        assertEquals(AssessmentOutcome.NILLED, assessment.getAssessmentOutcome());
+        assertEquals(BigDecimal.valueOf(1), assessment.getFixedFeeAmount());
+        assertEquals(BigDecimal.valueOf(2), assessment.getNetProfitCostsAmount());
+        assertEquals(BigDecimal.valueOf(3), assessment.getDisbursementAmount());
+        assertEquals(BigDecimal.valueOf(4), assessment.getDisbursementVatAmount());
+        assertEquals(BigDecimal.valueOf(5), assessment.getNetTravelCostsAmount());
+        assertEquals(BigDecimal.valueOf(6), assessment.getNetWaitingCostsAmount());
         assertEquals(true, assessment.getIsVatApplicable());
         assertEquals(userId, assessment.getCreatedByUserId());
     }
