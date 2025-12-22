21a9ce5590ab5ed3bfffc1473512c98d1ed4d22f
suma.challa <suma.challa@justice.gov.uk>
2025-12-22 13:29:37 +0000

    BC-288 Logout and Session expiry form local session and Force login when session not exist

---
+++ src/test/java/uk/gov/justice/laa/amend/claim/views/SessionExpiredViewTest.java
@@ -0,0 +1,28 @@
+package uk.gov.justice.laa.amend.claim.views;
+
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.justice.laa.amend.claim.config.LocalSecurityConfig;
+import uk.gov.justice.laa.amend.claim.controllers.LogoutController;
+
+@ActiveProfiles("local")
+@WebMvcTest(LogoutController.class)
+@Import(LocalSecurityConfig.class)
+public class SessionExpiredViewTest extends ViewTestBase {
+    protected SessionExpiredViewTest() {
+        super("/logout-success?message=expired");
+    }
+
+    @Test
+    void testPage() throws Exception {
+        Document doc = renderDocument();
+
+        assertPageHasTitle(doc, "You are now signed out of your account");
+        assertPageHasContent(doc, "For your security, we signed you out");
+        assertPageHasContent(doc, "This is because you were inactive for 15 minutes.");
+        assertPageHasPrimaryButton(doc, "Sign in");
+    }
+}
