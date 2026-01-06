a760fcef63e29710888052c187cd334452c162e4
Ben Millar <ben.millar@digital.justice.gov.uk>
2026-01-06 16:35:14 +0000

    BC-262: Fix security config syntax

--- src/main/java/uk/gov/justice/laa/amend/claim/config/LocalSecurityConfig.java
+++ src/main/java/uk/gov/justice/laa/amend/claim/config/LocalSecurityConfig.java
@@ -24,7 +24,7 @@ import java.time.Instant;
 import java.util.Collections;
 import java.util.Map;
 
-@Profile("local", "ephemeral")
+@Profile({"local", "ephemeral"})
 @Configuration
 @EnableWebSecurity
 public class LocalSecurityConfig {
