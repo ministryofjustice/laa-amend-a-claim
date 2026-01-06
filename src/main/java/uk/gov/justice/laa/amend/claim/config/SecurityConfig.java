607451744a06ca7f99c783465e33c932b3a94669
Ben Millar <ben.millar@digital.justice.gov.uk>
2026-01-06 16:18:21 +0000

    BC-262: Add ephemeral spring profile

--- src/main/java/uk/gov/justice/laa/amend/claim/config/SecurityConfig.java
+++ src/main/java/uk/gov/justice/laa/amend/claim/config/SecurityConfig.java
@@ -25,7 +25,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@Profile("!local")
+@Profile("!local & !ephemeral")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
