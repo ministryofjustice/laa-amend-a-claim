175e283228dd6a21a40cfdacc63d8828b4c83381
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2026-01-09 14:54:58 +0000

    BC-354: Ensuring a submission has happened in order to be able to access confirmation screen.

--- src/main/java/uk/gov/justice/laa/amend/claim/controllers/ConfirmationController.java
+++ src/main/java/uk/gov/justice/laa/amend/claim/controllers/ConfirmationController.java
@@ -1,10 +1,15 @@
 package uk.gov.justice.laa.amend.claim.controllers;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
+
+import static uk.gov.justice.laa.amend.claim.constants.AmendClaimConstants.ASSESSMENT_ID;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,13 +20,17 @@ public class ConfirmationController {
         Model model,
         @PathVariable(value = "submissionId") String submissionId,
         @PathVariable(value = "claimId") String claimId,
-        @PathVariable(value = "assessmentId") String assessmentId
+        @PathVariable(value = "assessmentId") String assessmentId,
+        HttpSession session
     ) {
-        // TODO check if assessment exists, otherwise return 404?
+        Object submittedAssessmentId = session.getAttribute(ASSESSMENT_ID);
+        if (submittedAssessmentId != null && submittedAssessmentId.toString().equals(assessmentId)) {
+            model.addAttribute("submissionId", submissionId);
+            model.addAttribute("claimId", claimId);
 
-        model.addAttribute("submissionId", submissionId);
-        model.addAttribute("claimId", claimId);
+            return "confirmation";
+        }
 
-        return "confirmation";
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 }
