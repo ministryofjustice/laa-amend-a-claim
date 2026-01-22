f1f929b247accf51c527144f7a38b84acf30e535
suma.challa <suma.challa@justice.gov.uk>
2026-01-22 09:24:37 +0000

    BC-353 modified e2e tests to use postgress from e2e-stack

--- src/e2e/main/java/uk/gov/justice/laa/amend/claim/config/EnvConfig.java
+++ src/e2e/main/java/uk/gov/justice/laa/amend/claim/config/EnvConfig.java
@@ -21,11 +21,7 @@ public class EnvConfig {
 
     public static boolean useMocks() { return Boolean.parseBoolean(getOrDefault("USE_MOCKS", "false")); }
 
-    public static String dbHost() { return getOrDefault("DB_HOST", "localhost"); }
-    public static int dbPort() {
-        return Integer.parseInt(getOrDefault("DB_PORT", "5440"));
-    }
-    public static String dbName() { return getOrDefault("DB_NAME", "postgres"); }
+    public static String dbConnectionUrl() { return getOrDefault("DB_CONNECTION_URL", "localhost"); }
     public static String dbUser() { return getOrDefault("DB_USER", "postgres"); }
     public static String dbPassword() { return getOrDefault("DB_PASSWORD", ""); }
     public static String userId() {
