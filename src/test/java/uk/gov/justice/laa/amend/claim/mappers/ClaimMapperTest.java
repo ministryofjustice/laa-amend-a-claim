af6078a9dfe25bb3983a26c1cdc2a15441c4a334
Ben Millar <ben.millar@justice.gov.uk>
2026-01-22 13:35:29 +0000

    BC-392: Map matterTypeCode consistently across crime/ civil claims (#155)

--- src/test/java/uk/gov/justice/laa/amend/claim/mappers/ClaimMapperTest.java
+++ src/test/java/uk/gov/justice/laa/amend/claim/mappers/ClaimMapperTest.java
@@ -704,7 +704,7 @@ class ClaimMapperTest {
         feeCalc.setBoltOnDetails(boltOn);
         response.setFeeCalculationResponse(feeCalc);
 
-        response.setCrimeMatterTypeCode("CRIME123");
+        response.setMatterTypeCode("INVC");
         response.setPoliceStationCourtPrisonId("PrisonCode");
         response.setSchemeId("SchemeId");
         CrimeClaimDetails claim = (CrimeClaimDetails) mapper.mapToClaimDetails(response, submissionResponse);
@@ -717,8 +717,7 @@ class ClaimMapperTest {
         assertEquals("FeeCodeDesc", claim.getFeeCodeDescription());
         assertEquals("Civil", claim.getCategoryOfLaw());
         assertTrue(claim.getEscaped());
-        assertEquals("CRIME123", claim.getMatterTypeCode());
-        assertEquals("CRIME123", claim.getMatterTypeCode());
+        assertEquals("INVC", claim.getMatterTypeCode());
         assertEquals("PrisonCode", claim.getPoliceStationCourtPrisonId());
         assertEquals("SchemeId", claim.getSchemeId());
 
