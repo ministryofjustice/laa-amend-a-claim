46b15a98655750290221f1c6d89c2d35eca788e2
suma.challa <suma.challa@justice.gov.uk>
2026-01-26 14:31:30 +0000

    BC-401 hide provider name

--- src/test/java/uk/gov/justice/laa/amend/claim/mappers/ClaimMapperTest.java
+++ src/test/java/uk/gov/justice/laa/amend/claim/mappers/ClaimMapperTest.java
@@ -617,7 +617,7 @@ class ClaimMapperTest {
         assertEquals("MT1+MT2", claim.getMatterTypeCode());
         assertEquals(claimSummaryFeeId.toString(), claim.getClaimSummaryFeeId());
         assertEquals("LEGAL HELP", claim.getAreaOfLaw());
-        assertEquals("User ID", claim.getProviderName());
+        assertNull(claim.getProviderName());
         assertEquals(LocalDateTime.of(2025, 1, 10, 14, 30, 0), claim.getSubmittedDate());
     }
 
@@ -663,7 +663,7 @@ class ClaimMapperTest {
         assertEquals("PrisonCode", claim.getPoliceStationCourtPrisonId());
         assertEquals("SchemeId", claim.getSchemeId());
 
-        assertEquals("User ID", claim.getProviderName());
+        assertNull(claim.getProviderName());
         assertEquals(LocalDateTime.of(2025, 1, 10, 14, 30, 0), claim.getSubmittedDate());
     }
 }
