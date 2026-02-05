676e69c98b3fe035656cf006a27abc43decedb38
spencer.howe <spencer.howe@justice.gov.uk>
2026-02-05 14:00:54 +0000

    feat: return to using error screen

--- src/main/java/uk/gov/justice/laa/amend/claim/exceptions/ErrorPageController.java
+++ src/main/java/uk/gov/justice/laa/amend/claim/exceptions/ErrorPageController.java
@@ -3,16 +3,53 @@ package uk.gov.justice.laa.amend.claim.exceptions;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.webmvc.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static reactor.netty.http.HttpConnectionLiveness.log;
 
 @Controller
 public class ErrorPageController implements ErrorController {
 
     @RequestMapping("/error")
-    public String handleError(HttpServletRequest request, Model model) {
+    public String handleError(HttpServletRequest request, Model model) throws IOException {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Path message = Paths.get("/config/maintenance/message");
+        Path title = Paths.get("/config/maintenance/title");
+
+        Path enabled = Paths.get("/config/maintenance/enabled");
+
+        if (Files.exists(enabled)
+                && Files.readString(enabled).replace("/r", "")
+                .trim().equalsIgnoreCase("true")) {
+
+
+            model.addAttribute( "message",Files.readString(message));
+            model.addAttribute( "title",Files.readString(title));
+
+            log.error("============= in error controller");
+            log.error("============= in error controller");
+            log.error("============= in error controller");
+
+
+            log.error("MESSAGE: " , Files.readString(message).trim());
+            log.error("TITLE: " , Files.readString(title).trim());
+
+            log.error("============= in error controller");
+            log.error("============= in error controller");
+            log.error("============= in error controller");
+
+        }
+
+
+
         if (status != null) {
             int statusCode = Integer.parseInt(status.toString());
             model.addAttribute("statusCode", statusCode);
--- src/main/java/uk/gov/justice/laa/amend/claim/interceptors/MaintenanceInterceptor.java
+++ src/main/java/uk/gov/justice/laa/amend/claim/interceptors/MaintenanceInterceptor.java
@@ -20,77 +20,29 @@ import java.nio.file.Paths;
 public class MaintenanceInterceptor implements HandlerInterceptor {
 
     Path enabled = Paths.get("/config/maintenance/enabled");
-    Path message = Paths.get("/config/maintenance/message");
-    Path title = Paths.get("/config/maintenance/title");
 
     @Override
     public boolean preHandle(
             HttpServletRequest request,
             HttpServletResponse response,
             Object handler) throws IOException, ServletException {
-        log.error("============1");
-        log.error("============1");
         log.error("entering maintenance interceptor");
-        log.error("============2");
-        log.error("============2");
 
-        String enabledValue = new String(
-                Files.readAllBytes(enabled), StandardCharsets.UTF_8
-        ).trim();
-
-        log.error("RAW ENABLED = {}", enabledValue);
-
-        boolean enabledBoolean = Boolean.parseBoolean(enabledValue);
-
-        log.error("parsed ENABLED = {}", enabledBoolean);
-
-
-        log.error("============PATH");
-        log.error("============PATH");
-        log.error("Path String: {}", enabled);
-        log.error("File exists?: {}", Files.exists(enabled));
-        log.error("Absolute path: {}", enabled.toAbsolutePath());
-        log.error("Readable: {}", Files.isReadable(enabled));
-        log.error("============PATHEND");
-        log.error("============PATHEND");
 
         String path = request.getRequestURI();
         if (path.startsWith("/actuator") || path.startsWith("/health")) {
-
-            log.error("============3");
-            log.error("============3");
-            log.error("inside actuator or health");
-            log.error("============4");
-            log.error("============4");
             return true;
-
-        }
-
-        if (Files.exists(enabled)) {
-            log.error("============EC");
-            log.error("============EC");
-            log.error("enabled found");
-            log.error(Files.readString(enabled));
-            log.error("============End");
-            log.error("============End");
         }
 
         if (Files.exists(enabled)
                 && Files.readString(enabled).replace("/r", "")
                 .trim().equalsIgnoreCase("true")) {
 
-            log.error("============5");
-            log.error("============5");
-            log.error("if file exists hit");
-            log.error("============6");
-            log.error("============6");
-
             request.setAttribute(RequestDispatcher.ERROR_STATUS_CODE, 503);
-            request.setAttribute(RequestDispatcher.ERROR_MESSAGE, Files.readString(message).trim());
 
             response.setStatus(HttpStatus.SERVICE_UNAVAILABLE.value());
 
-            request.getRequestDispatcher("/maintenance").forward(request, response);
+            request.getRequestDispatcher("/error").forward(request, response);
 
             return false;
         }
