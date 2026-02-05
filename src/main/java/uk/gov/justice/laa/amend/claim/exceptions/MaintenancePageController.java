34b86bea73feeb032d917fd1e848cc857fdaecf0
spencer.howe <spencer.howe@justice.gov.uk>
2026-02-05 13:46:54 +0000

    feat: add controller

---
+++ src/main/java/uk/gov/justice/laa/amend/claim/exceptions/MaintenancePageController.java
@@ -0,0 +1,22 @@
+package uk.gov.justice.laa.amend.claim.exceptions;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.webmvc.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MaintenancePageController implements ErrorController {
+
+    @RequestMapping("/maintenance")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+            model.addAttribute("statusCode", statusCode);
+        }
+        return "maintenance";
+    }
+}
