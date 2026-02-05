f2eb2c006db465687eb8808b8c55783299ddbe32
spencer.howe <spencer.howe@justice.gov.uk>
2026-02-05 14:18:08 +0000

    feat: add error to url exclusions

--- src/main/java/uk/gov/justice/laa/amend/claim/config/WebConfig.java
+++ src/main/java/uk/gov/justice/laa/amend/claim/config/WebConfig.java
@@ -21,6 +21,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new MaintenanceInterceptor())
                 .order(Ordered.HIGHEST_PRECEDENCE)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/actuator/**", "/health", "/maintenance");
+                .excludePathPatterns("/actuator/**", "/health", "/error");
     }
 }
