eecef9d2570f0f44f6ca2b889b0b1410ca8dda3b
spencer.howe <spencer.howe@justice.gov.uk>
2026-02-16 16:06:56 +0000

    feat: remove redundant method

--- src/main/java/uk/gov/justice/laa/amend/claim/viewmodels/ThymeleafLiteralString.java
+++ src/main/java/uk/gov/justice/laa/amend/claim/viewmodels/ThymeleafLiteralString.java
@@ -14,8 +14,4 @@ public class ThymeleafLiteralString extends ThymeleafString {
     public String resolve(Messages messages) {
         return value;
     }
-
-    public String convertThymeleafMessage(ThymeleafMessage message) {
-        return message.toString();
-    }
 }
