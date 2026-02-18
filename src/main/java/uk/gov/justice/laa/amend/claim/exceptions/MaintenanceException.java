27d55b84ddd143aaddf4651040014952e7e34776
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2026-02-18 15:42:28 +0000

    Removing maintenance page controller.

--- src/main/java/uk/gov/justice/laa/amend/claim/exceptions/MaintenanceException.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.justice.laa.amend.claim.exceptions;
-
-public class MaintenanceException extends Exception {
-    public MaintenanceException(final String message) {
-        super(message);
-    }
-}
