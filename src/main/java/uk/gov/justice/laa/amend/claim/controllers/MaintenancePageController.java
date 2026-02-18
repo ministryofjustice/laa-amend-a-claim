ce49334c8e2f1285cb46be4053126f98a1e93e58
spencer.howe <spencer.howe@justice.gov.uk>
2026-02-18 16:56:42 +0000

    feat: change maintenance mode to redirect

--- src/main/java/uk/gov/justice/laa/amend/claim/controllers/MaintenancePageController.java
+++ src/main/java/uk/gov/justice/laa/amend/claim/controllers/MaintenancePageController.java
@@ -1,5 +1,7 @@
 package uk.gov.justice.laa.amend.claim.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,9 +15,9 @@ public class MaintenancePageController {
     private final MaintenanceService maintenanceService;
 
     @GetMapping("/maintenance")
-    public String onPageLoad(Model model) {
+    public String onPageLoad(Model model, HttpServletRequest request) throws IOException {
 
-        if (!maintenanceService.maintenanceEnabled()) {
+        if (!maintenanceService.maintenanceApplies(request)) {
             return "redirect:/";
         }
 
