0cb770d85e13e18d21ca13ca19b9d99d270dcceb
Michael Boyd <michael.boyd1@justice.gov.uk>
2026-02-26 16:59:56 +0000

    BC-382: Use env variables instead

--- e2e.sh
+++ e2e.sh
@@ -25,7 +25,7 @@ export CLAIMS_API="http://localhost:8082"
 export CLAIMS_TOKEN="f67f968e-b479-4e61-b66e-f57984931e56"
 export SPRING_PROFILES_ACTIVE="e2e"
 export SILAS_AUTH_ENABLED=false
-export FEATURE_FLAGS_ROOT=config/local/featureFlags
+export IS_VOIDING_ENABLED=true
 
 echo "[INFO] Starting API application..."
 pushd ../laa-data-claims-api >/dev/null
