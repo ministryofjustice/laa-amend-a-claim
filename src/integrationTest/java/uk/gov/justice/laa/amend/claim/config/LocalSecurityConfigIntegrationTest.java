996fbeed05785cfbbd588a3d810a04e7af73c4f2
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2026-03-05 09:37:28 +0000

    BC-461: Ignoring Provate IP disclosure alert. Updated servlet config.

--- src/integrationTest/java/uk/gov/justice/laa/amend/claim/config/LocalSecurityConfigIntegrationTest.java
+++ src/integrationTest/java/uk/gov/justice/laa/amend/claim/config/LocalSecurityConfigIntegrationTest.java
@@ -1,10 +1,13 @@
 package uk.gov.justice.laa.amend.claim.config;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static uk.gov.justice.laa.amend.claim.config.security.SecurityConstants.PERMISSIONS_POLICY;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,9 +27,20 @@ public class LocalSecurityConfigIntegrationTest extends RedisSetup {
 
     @ParameterizedTest
     @ValueSource(strings = {"/", "/js/app.js", "/css/styles.css"})
-    void responseHasCorrectHeaders() throws Exception {
-        mockMvc.perform(get("/"))
+    void responseOnGetHasCorrectHeaders(String url) throws Exception {
+        mockMvc.perform(get(url))
                 .andExpect(status().isOk())
+                .andExpect(header().string("Cross-Origin-Opener-Policy", "same-origin"))
+                .andExpect(header().string("Cross-Origin-Embedder-Policy", "require-corp"))
+                .andExpect(header().string("Cross-Origin-Resource-Policy", "same-origin"))
+                .andExpect(header().string("X-Content-Type-Options", "nosniff"))
+                .andExpect(header().string("Permissions-Policy", PERMISSIONS_POLICY));
+    }
+
+    @Test
+    void responseOnPostHasCorrectHeaders() throws Exception {
+        mockMvc.perform(post("/").with(csrf()))
+                .andExpect(status().isBadRequest())
                 .andExpect(header().string("Cross-Origin-Opener-Policy", "same-origin"))
                 .andExpect(header().string("Cross-Origin-Embedder-Policy", "require-corp"))
                 .andExpect(header().string("Cross-Origin-Resource-Policy", "same-origin"))
