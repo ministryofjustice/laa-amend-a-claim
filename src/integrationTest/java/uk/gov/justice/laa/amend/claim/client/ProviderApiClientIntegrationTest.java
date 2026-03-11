ca71b31b723842ab7e521c764af273137784b050
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2026-03-11 12:20:35 +0000

    BC-497: Added 503 and DOWN tests.

--- src/integrationTest/java/uk/gov/justice/laa/amend/claim/client/ProviderApiClientIntegrationTest.java
+++ src/integrationTest/java/uk/gov/justice/laa/amend/claim/client/ProviderApiClientIntegrationTest.java
@@ -1,10 +1,18 @@
 package uk.gov.justice.laa.amend.claim.client;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.health.contributor.Status;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import uk.gov.justice.laa.amend.claim.base.WireMockSetup;
+import uk.gov.justice.laa.amend.claim.models.HealthDto;
 
 @SpringBootTest
 public class ProviderApiClientIntegrationTest extends WireMockSetup {
@@ -13,11 +21,39 @@ public class ProviderApiClientIntegrationTest extends WireMockSetup {
     private ProviderApiClient providerApiClient;
 
     @Test
-    void testPingWhenOkResponse() {
+    void testPingWhenUpResponse() {
         setupGetProviderDetailsApiHealthStub();
 
         Assertions.assertDoesNotThrow(() -> providerApiClient.ping().block());
     }
 
-    // TODO add integration test with status: DOWN response
+    @Test
+    void testPingWhenDownResponse() {
+        String response = """
+            {
+                "status": "DOWN"
+            }\
+            """;
+
+        stubFor(get(urlPathMatching("/api/v1/actuator/health"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/vnd.spring-boot.actuator.v3+json")
+                        .withBody(response)));
+
+        HealthDto health = providerApiClient.ping().block();
+
+        Assertions.assertNotNull(health);
+        Assertions.assertEquals(Status.DOWN, health.getStatus());
+    }
+
+    @Test
+    void testPingWhenServiceUnavailableResponse() {
+        stubFor(get(urlPathMatching("/api/v1/actuator/health"))
+                .willReturn(aResponse().withStatus(503)));
+
+        Assertions.assertThrows(
+                WebClientResponseException.class, () -> providerApiClient.ping().block());
+        ;
+    }
 }
