a8588555302b68d071e798137c8b6be0d7e2730b
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2026-03-13 10:49:31 +0000

    BC-506: Migrated to scss and introduced govuk-table--small-text class.

--- src/integrationTest/java/uk/gov/justice/laa/amend/claim/config/SecurityConfigIntegrationTest.java
+++ src/integrationTest/java/uk/gov/justice/laa/amend/claim/config/SecurityConfigIntegrationTest.java
@@ -104,7 +104,7 @@ public class SecurityConfigIntegrationTest extends RedisSetup {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"/", "/js/app.js", "/css/styles.css"})
+    @ValueSource(strings = {"/", "/js/app.js", "/css/styles.scss"})
     @WithMockUser(roles = "USER")
     void responseOnGetHasCorrectHeaders(String url) throws Exception {
         mockMvc.perform(get(url))
