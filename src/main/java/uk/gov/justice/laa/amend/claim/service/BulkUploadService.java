490279d916a0d69f9ba61bdd7f306bc5d1e8a2c0
Michael Boyd <michael.boyd1@justice.gov.uk>
2026-03-25 09:02:48 +0000

    BC-535: Submit a stub assessment

--- src/main/java/uk/gov/justice/laa/amend/claim/service/BulkUploadService.java
+++ src/main/java/uk/gov/justice/laa/amend/claim/service/BulkUploadService.java
@@ -1,5 +1,20 @@
 package uk.gov.justice.laa.amend.claim.service;
 
+import static uk.gov.justice.laa.amend.claim.constants.AmendClaimConstants.Label.ADJOURNED_FEE;
+import static uk.gov.justice.laa.amend.claim.constants.AmendClaimConstants.Label.ALLOWED_TOTAL_INCL_VAT;
+import static uk.gov.justice.laa.amend.claim.constants.AmendClaimConstants.Label.ALLOWED_TOTAL_VAT;
+import static uk.gov.justice.laa.amend.claim.constants.AmendClaimConstants.Label.ASSESSED_TOTAL_INCL_VAT;
+import static uk.gov.justice.laa.amend.claim.constants.AmendClaimConstants.Label.ASSESSED_TOTAL_VAT;
+import static uk.gov.justice.laa.amend.claim.constants.AmendClaimConstants.Label.CMRH_ORAL;
+import static uk.gov.justice.laa.amend.claim.constants.AmendClaimConstants.Label.CMRH_TELEPHONE;
+import static uk.gov.justice.laa.amend.claim.constants.AmendClaimConstants.Label.COUNSELS_COST;
+import static uk.gov.justice.laa.amend.claim.constants.AmendClaimConstants.Label.DETENTION_TRAVEL_COST;
+import static uk.gov.justice.laa.amend.claim.constants.AmendClaimConstants.Label.DISBURSEMENT_VAT;
+import static uk.gov.justice.laa.amend.claim.constants.AmendClaimConstants.Label.HO_INTERVIEW;
+import static uk.gov.justice.laa.amend.claim.constants.AmendClaimConstants.Label.JR_FORM_FILLING;
+import static uk.gov.justice.laa.amend.claim.constants.AmendClaimConstants.Label.NET_DISBURSEMENTS_COST;
+import static uk.gov.justice.laa.amend.claim.constants.AmendClaimConstants.Label.NET_PROFIT_COST;
+import static uk.gov.justice.laa.amend.claim.constants.AmendClaimConstants.Label.SUBSTANTIVE_HEARING;
 import static uk.gov.justice.laa.amend.claim.models.BulkUploadResult.BulkUploadStatus.PARSING_FAILURE;
 import static uk.gov.justice.laa.amend.claim.models.BulkUploadResult.BulkUploadStatus.SUBMISSION_FAILURE;
 import static uk.gov.justice.laa.amend.claim.models.BulkUploadResult.BulkUploadStatus.SUCCESS;
@@ -7,10 +22,12 @@ import static uk.gov.justice.laa.amend.claim.models.BulkUploadResult.BulkUploadS
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.IntStream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.csv.CSVFormat;
@@ -21,7 +38,16 @@ import org.springframework.web.multipart.MultipartFile;
 import uk.gov.justice.laa.amend.claim.bulkupload.CsvHeaderValidator;
 import uk.gov.justice.laa.amend.claim.bulkupload.CsvRowMapper;
 import uk.gov.justice.laa.amend.claim.bulkupload.CsvSchemaProvider;
+import uk.gov.justice.laa.amend.claim.models.AllowedClaimField;
+import uk.gov.justice.laa.amend.claim.models.AssessedClaimField;
+import uk.gov.justice.laa.amend.claim.models.BoltOnClaimField;
 import uk.gov.justice.laa.amend.claim.models.BulkUploadResult;
+import uk.gov.justice.laa.amend.claim.models.CivilClaimDetails;
+import uk.gov.justice.laa.amend.claim.models.ClaimDetails;
+import uk.gov.justice.laa.amend.claim.models.Cost;
+import uk.gov.justice.laa.amend.claim.models.CostClaimField;
+import uk.gov.justice.laa.amend.claim.models.FixedFeeClaimField;
+import uk.gov.justice.laa.amend.claim.models.OutcomeType;
 import uk.gov.justice.laa.amend.claim.models.ClaimDetails;
 
 @RequiredArgsConstructor
@@ -93,13 +119,13 @@ public abstract class BulkUploadService<T> {
         return new BulkUploadResult(SUCCESS, List.of());
     }
 
-    protected BulkUploadResult submit(List<ClaimDetails> claimDetails, UUID userId) {
+    protected BulkUploadResult submit(List<? extends ClaimDetails> claimDetails, UUID userId) {
         for (int row = 0; row < claimDetails.size(); ++row) {
             try {
                 var claim = claimDetails.get(row);
                 log.info(
                         "Bulk upload in progress. Submitting assessment for row {}, claim {}, UFN {}",
-                        row + ROW_OFFSET,
+                        row,
                         claim.getClaimId(),
                         claim.getUniqueFileNumber());
                 assessmentService.submitAssessment(claim, userId.toString());
@@ -107,7 +133,7 @@ public abstract class BulkUploadService<T> {
                 var message = String.format(
                         "Row %s: Failed to submit assessment. %s prior rows in the file have already been"
                                 + " processed and do not need to be reuploaded.",
-                        row + ROW_OFFSET, row);
+                        row + 1, row);
                 log.error(message, ex);
                 return new BulkUploadResult(SUBMISSION_FAILURE, List.of(message));
             }
@@ -116,4 +142,5 @@ public abstract class BulkUploadService<T> {
         log.info(successMessage);
         return new BulkUploadResult(SUCCESS, List.of(successMessage));
     }
+
 }
