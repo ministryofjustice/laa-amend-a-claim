8906681a8f08578acded61fe0c352122d02b7ae5
Michael Boyd <michael.boyd1@justice.gov.uk>
2026-04-02 09:59:32 +0100

    BC-483: Gather parsing errors

--- src/main/java/uk/gov/justice/laa/amend/claim/bulkupload/CsvRowMapper.java
+++ src/main/java/uk/gov/justice/laa/amend/claim/bulkupload/CsvRowMapper.java
@@ -1,7 +1,8 @@
 package uk.gov.justice.laa.amend.claim.bulkupload;
 
+import java.util.List;
 import org.apache.commons.csv.CSVRecord;
 
 public interface CsvRowMapper<T> {
-    T mapRow(CSVRecord record, int rowNumber);
+    T mapRow(CSVRecord record, int rowNumber, List<BulkUploadError> errors);
 }
