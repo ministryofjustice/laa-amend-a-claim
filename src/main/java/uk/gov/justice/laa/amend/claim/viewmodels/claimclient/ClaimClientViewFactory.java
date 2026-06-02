6d7db92b09064f30b78066f77ca4f7fc6a937809
gabrielageorgieva <gabriela.georgieva@justice.gov.uk>
2026-06-02 16:38:06 +0100

    visible client details tab

--- src/main/java/uk/gov/justice/laa/amend/claim/viewmodels/claimclient/ClaimClientViewFactory.java
+++ src/main/java/uk/gov/justice/laa/amend/claim/viewmodels/claimclient/ClaimClientViewFactory.java
@@ -4,6 +4,7 @@ import lombok.experimental.UtilityClass;
 import uk.gov.justice.laa.amend.claim.models.CivilClaimDetails;
 import uk.gov.justice.laa.amend.claim.models.ClaimDetails;
 import uk.gov.justice.laa.amend.claim.models.CrimeClaimDetails;
+import uk.gov.justice.laa.amend.claim.viewmodels.CivilClaimClientView;
 
 @UtilityClass
 public class ClaimClientViewFactory {
@@ -11,8 +12,7 @@ public class ClaimClientViewFactory {
   public static ClaimClientView create(ClaimDetails claim) {
     return switch (claim) {
       case CrimeClaimDetails crimeClaim -> new CrimeClaimClientView(crimeClaim);
-      case CivilClaimDetails civilClaimDetails ->
-          throw new IllegalArgumentException("TODO: BC-567 will implement this");
+      case CivilClaimDetails civilClaimDetails -> new CivilClaimClientView(civilClaimDetails);
       default -> throw new IllegalArgumentException("Unsupported ClaimDetails type");
     };
   }
