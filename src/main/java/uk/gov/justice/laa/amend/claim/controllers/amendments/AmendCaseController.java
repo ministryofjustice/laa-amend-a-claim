f74fb61f28f827939aa9d96062c78c393ef2c71d
Jamie Briggs <jamie.briggs@justice.gov.uk>
2026-06-18 15:05:37 +0100

    BC-569 | Initial implementation of AmendCaseController and view-case.html

---
+++ src/main/java/uk/gov/justice/laa/amend/claim/controllers/amendments/AmendCaseController.java
@@ -0,0 +1,45 @@
+package uk.gov.justice.laa.amend.claim.controllers.amendments;
+
+import static uk.gov.justice.laa.amend.claim.utils.SessionUtils.getAmendmentForms;
+import static uk.gov.justice.laa.amend.claim.utils.SessionUtils.getValidAmendableClaim;
+
+import jakarta.servlet.http.HttpSession;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import uk.gov.justice.laa.amend.claim.annotations.HasRoleClaimAmendmentsCaseworker;
+import uk.gov.justice.laa.amend.claim.config.FeatureFlagsConfig;
+import uk.gov.justice.laa.amend.claim.viewmodels.claimcase.ClaimCaseViewFactory;
+
+@Controller
+@RequestMapping("/submissions/{submissionId}/claims/{claimId}/amendments")
+@RequiredArgsConstructor
+@HasRoleClaimAmendmentsCaseworker
+public class AmendCaseController {
+
+  private final FeatureFlagsConfig featureFlagsConfig;
+
+  @GetMapping("/case")
+  public String viewCase(
+      HttpSession session,
+      Model model,
+      @PathVariable UUID submissionId,
+      @PathVariable UUID claimId) {
+    featureFlagsConfig.checkClaimAmendmentEnabled();
+
+    var claim = getValidAmendableClaim(session, submissionId, claimId);
+    var amendmentForms = getAmendmentForms(session, claimId);
+
+    var claimView = ClaimCaseViewFactory.create(claim);
+    model.addAttribute("claim", claimView);
+
+    model.addAttribute("client1Form", amendmentForms.getClient1Form().getCurrent());
+    model.addAttribute("forms", amendmentForms);
+
+    return "amendments/view-case";
+  }
+}
