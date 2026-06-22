ccad4b748e72511cf7d0c906dc3c517f18e4f265
Michael Boyd <michael.boyd1@justice.gov.uk>
2026-06-22 11:46:42 +0100

    BC-568: Avoid null feature flags

--- src/e2e/main/java/uk/gov/justice/laa/amend/claim/pages/ClaimDetailsPage.java
+++ src/e2e/main/java/uk/gov/justice/laa/amend/claim/pages/ClaimDetailsPage.java
@@ -1,7 +1,6 @@
 package uk.gov.justice.laa.amend.claim.pages;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
-import static uk.gov.justice.laa.amend.claim.helpers.PageHelper.assertSummaryListValue;
 import static uk.gov.justice.laa.amend.claim.helpers.PageHelper.cardByTitle;
 import static uk.gov.justice.laa.amend.claim.helpers.PageHelper.summaryListRowByLabel;
 import static uk.gov.justice.laa.amend.claim.helpers.PageHelper.tableRowByLabel;
