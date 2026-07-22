3cdb2524c8d1eb02434f8c018bc41169eb54179d
Jamie Briggs <jamie.briggs@justice.gov.uk>
2026-07-22 13:27:57 +0100

    BC-694 | Update pact given for non-existent claim

--- src/pactTest/java/uk/gov/justice/laa/amend/claim/ClaimsGetAssessmentsPactTest.java
+++ src/pactTest/java/uk/gov/justice/laa/amend/claim/ClaimsGetAssessmentsPactTest.java
@@ -76,7 +76,7 @@ public final class ClaimsGetAssessmentsPactTest extends AbstractPactTest {
   @Pact(consumer = CONSUMER)
   public RequestResponsePact getAssessments404(PactDslWithProvider builder) {
     return builder
-        .given("no assessments exist for the claim")
+        .given("no claim exists")
         .uponReceiving("a request to get assessments for a non-existent claim")
         .matchPath(
             "/api/v1/claims/(" + UUID_REGEX + ")/assessments",
