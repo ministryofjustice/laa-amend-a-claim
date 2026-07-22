a2382f163bdf50546d21d99c803e01fab21b9787
Jamie Briggs <jamie.briggs@justice.gov.uk>
2026-07-22 13:26:31 +0100

    BC-694 | Removing matter type properties from AmendFeeCodeViewTest

--- src/test/java/uk/gov/justice/laa/amend/claim/views/amendments/AmendFeeCodeViewTest.java
+++ src/test/java/uk/gov/justice/laa/amend/claim/views/amendments/AmendFeeCodeViewTest.java
@@ -22,8 +22,6 @@ class AmendFeeCodeViewTest extends AmendmentsBaseTest {
 
   private static final String FEE_CODE = "feecode";
   private static final String MATTER_TYPE_CODE = "matter";
-  private static final String MATTER_TYPE_CODE_1 = "matterone";
-  private static final String MATTER_TYPE_CODE_2 = "mattertwo";
 
   @MockitoBean AvailableFeeCodesService availableFeeCodesService;
 
