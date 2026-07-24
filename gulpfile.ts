9d44da5de10844f7d10c4136777c6d36e98db83e
Jamie Briggs <personal@jbriggs.dev>
2026-07-24 15:49:15 +0100

    refactor: Introduce gulpfile.ts (#478)
    * Started on gulpfile.js
    
    * Added copy JS job to gulpfile.js
    
    * Updated references on frontend to remove webjars in favour of stack resources
    
    * Added gulp watch task
    
    * Switch to typescript
    
    * Updated to compile javascript into minified file
    
    * Add accessible autocomplete to gulpfile.ts and app.scss
    
    * Deleted static resources, added to .gitignore
    
    * Added gulp build step to build.yml
    
    * BC-687 | Updated integration tests to check correct static resources
    
    * Addressing PR comments
    
    * Added npm gulp to e2e-tests.yml
    
    * Added README.md
    
    * Updated Dockerfile.e2e-runner to use NODE_VERSION 24
    
    * BC-694 | Adjusted for running gulp via gradle task
    
    * BC-694 | Documentation added
    
    * BC-694 | Added additional destinations for static resource pipelines so jar can be build properly
    
    * Simplified gulpfile.ts and made fe assets copy to jar file
    
    * Added npm configuration to Dependabot for daily updates with a 7-day cooldown
    
    * Added 'displayMenu: overlay' option to dropdown initialization configuration
    
    * Changes following comments

---
+++ gulpfile.ts
@@ -0,0 +1,135 @@
+import gulp, {series, task} from 'gulp';
+import rename from 'gulp-rename';
+import cleanCSS from 'gulp-clean-css';
+import gulpSass from 'gulp-sass';
+import * as dartSass from 'sass';
+import browserSyncModule from 'browser-sync';
+import type {Transform} from 'stream';
+import gulpTypescript from 'gulp-typescript';
+import terser from 'gulp-terser';
+
+const sass = gulpSass(dartSass);
+const browserSync = browserSyncModule.create();
+
+const tsProject = gulpTypescript.createProject('tsconfig.json');
+
+function compileStylesheets() {
+  return gulp.src('src/main/resources/sass/app.scss')
+  .pipe(sass.sync({
+        // Set to project root and node_modules
+        // on purpose to make moj stylesheet find gov uk references
+        // properly within node_modules.
+        loadPaths: ['.', 'node_modules'],
+        quietDeps: true,        // silences the @import deprecation noise from vendor files
+        silenceDeprecations: ['import']
+      })
+      .on('error', function (this: Transform, err: Error) {
+        // @ts-ignore: Usually shouldn't suppress, however this is a dev file and not for production.
+        sass.logError.call(this, err);   // print the readable Sass error
+        this.emit('end');                // end the stream so Gulp doesn't hang
+      })
+  )
+  .pipe(cleanCSS())
+  .pipe(rename('app.min.css'))
+  .pipe(gulp.dest('build/generatedFEAssets/static/css'))
+  .on('end', () => console.log('CSS written to build/generatedFEAssets/static/css'));
+}
+
+function copyGOVUKJavascript() {
+  return gulp.src('node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.js')
+  .pipe(gulp.dest('build/generatedFEAssets/static/js'))
+  .pipe(gulp.dest('src/main/resources/es'))
+  .on('end', () => console.log('GOV.UK JS copied to build/generatedFEAssets/static/js and src/main/resources/es'));
+}
+
+function copyGOVUKAutocompleteJavascript() {
+  return gulp.src('node_modules/accessible-autocomplete/dist/accessible-autocomplete.min.js')
+  .pipe(gulp.dest('build/generatedFEAssets/static/js'))
+  .pipe(gulp.dest('src/main/resources/es'))
+  .on('end', () => console.log('GOV.UK Autocomplete JS copied to build/generatedFEAssets/static/js and src/main/resources/es'));
+
+}
+
+function copyMOJJavascript() {
+  return gulp.src('node_modules/@ministryofjustice/frontend/moj/moj-frontend.min.js')
+  .pipe(gulp.dest('build/generatedFEAssets/static/js'))
+  .pipe(gulp.dest('src/main/resources/es'))
+  .on('end', () => console.log('MOJ JS copied to build/generatedFEAssets/static/js and src/main/resources/es'));
+}
+
+function copyGOVUKAssets() {
+  return gulp.src('node_modules/govuk-frontend/dist/govuk/assets/**/*')
+  .pipe(gulp.dest('build/generatedFEAssets/static/assets'))
+  .on('end', () => console.log('GOV.UK assets copied to build/generatedFEAssets/static/assets'));
+}
+
+function copyMOJAssets() {
+  return gulp.src('node_modules/@ministryofjustice/frontend/moj/assets/**/*')
+  .pipe(gulp.dest('build/generatedFEAssets/static/assets'))
+  .on('end', () => console.log('MOJ assets copied to build/generatedFEAssets/static/assets'));
+}
+
+function compileScripts() {
+  return gulp.src([
+    'src/main/resources/es/**/*.ts',
+    'src/main/resources/es/**/*.d.ts'
+  ])
+  .pipe(tsProject())
+  .pipe(terser())
+  .pipe(rename('app.min.js'))
+  .pipe(gulp.dest('build/generatedFEAssets/static/js'))
+  .on('end', () => console.log('App JS compiled to build/generatedFEAssets/static/js'));
+}
+
+function watch() {
+  browserSync.init({
+    proxy: {
+      target: 'localhost:8090',
+      proxyReq: [
+        function (proxyReq: any) {
+          // ask the backend not to compress, so BrowserSync can inject the snippet
+          proxyReq.setHeader('Accept-Encoding', 'identity');
+        }
+      ],
+      proxyRes: [
+        function (proxyRes: any) {
+          // strip CSP so BrowserSync's inline client script is allowed (DEV ONLY)
+          delete proxyRes.headers['content-security-policy'];
+          delete proxyRes.headers['content-security-policy-report-only'];
+        }
+      ]
+    },
+    open: true,
+    notify: false
+  });
+
+  gulp.watch([
+    'src/main/resources/templates/**',
+    'src/main/resources/templates/**/*'
+  ])
+  .on('change', (path) => {
+    console.log('Template changed:', path);
+    browserSync.reload();
+  });
+
+  gulp.watch('src/main/resources/sass/**/*.scss', series(compileStylesheets))
+  .on('change', (path) => {
+    console.log('Stylesheet changed:', path);
+    browserSync.reload();
+  });
+
+  gulp.watch('src/main/resources/es/**/*.{ts,js}', series(compileScripts))
+  .on('change', (path) => {
+    console.log('Script changed:', path);
+    browserSync.reload();
+  });
+
+}
+
+task('copy-assets', series(copyGOVUKAssets, copyMOJAssets));
+task('copy-js', series(copyGOVUKJavascript, copyGOVUKAutocompleteJavascript, copyMOJJavascript));
+task('compile-stylesheets', compileStylesheets);
+task('compile-scripts', series('copy-js', compileScripts));
+
+task('default', series('copy-assets', 'compile-stylesheets', 'compile-scripts'));
+task('watch', series(watch));
---
+++ src/main/resources/es/app.ts
@@ -0,0 +1,45 @@
+import * as GOVUKFrontend from './govuk-frontend.min.js';
+import * as MOJFrontend from './moj-frontend.min.js';
+
+document.body.className += ` js-enabled${'noModule' in HTMLScriptElement.prototype ? ' govuk-frontend-supported' : ''}`;
+
+GOVUKFrontend.initAll();
+MOJFrontend.initAll();
+
+document.querySelectorAll<HTMLAnchorElement>('[data-module="back-link"]').forEach(function (link: HTMLAnchorElement): void {
+    link.addEventListener('click', function (e: MouseEvent): void {
+        e.preventDefault();
+        window.history.back();
+    });
+});
+
+document.querySelectorAll<HTMLAnchorElement>('[data-module="logout-link"]').forEach(function (link: HTMLAnchorElement): void {
+    link.addEventListener('click', function (e: MouseEvent): void {
+        e.preventDefault();
+        (document as any).logoutForm.submit();
+    });
+});
+
+
+document.querySelectorAll<HTMLFormElement>('[id=bulk-upload-form]').forEach(function (form: HTMLFormElement): void {
+    form.addEventListener('submit', function (): void {
+        (form.querySelector('#bulk-upload-button') as HTMLButtonElement).disabled = true;
+        (form.querySelector('#loading-wrapper') as HTMLElement).style.display = "block";
+        (form.querySelector('#drop-zone') as HTMLElement).style.display = "none";
+    });
+});
+
+
+const selectDropdowns = document.querySelectorAll<HTMLSelectElement>('[data-module="make-autocomplete"]');
+
+// For each dropdown
+selectDropdowns.forEach(function (select: HTMLSelectElement) {
+    accessibleAutocomplete.enhanceSelectElement({
+        element: select,
+        id: select.id,
+        defaultValue: select.options[select.options.selectedIndex].innerHTML,
+        selectElement: select,
+        allowEmpty: true,
+        displayMenu: 'overlay'
+    });
+});
