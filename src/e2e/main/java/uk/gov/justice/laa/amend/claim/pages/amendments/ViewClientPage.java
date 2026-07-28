4ead6a66931155cfafb0e417595bd00a2d32d99c
Jamie Briggs <jamie.briggs@justice.gov.uk>
2026-07-28 18:44:16 +0100

    BC-601 | Added UI and E2E tests for amending client 2 details

--- src/e2e/main/java/uk/gov/justice/laa/amend/claim/pages/amendments/ViewClientPage.java
+++ src/e2e/main/java/uk/gov/justice/laa/amend/claim/pages/amendments/ViewClientPage.java
@@ -2,18 +2,23 @@ package uk.gov.justice.laa.amend.claim.pages.amendments;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.options.AriaRole;
 
 public class ViewClientPage extends BaseAmendmentPage {
 
-  private final Locator changeLink;
+  private final Locator changeClientOneLine;
+  private final Locator changeClientTwoLink;
 
   public ViewClientPage(Page page) {
     super(page);
-    this.changeLink = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Change"));
+    this.changeClientOneLine = page.locator("#amend-client-1");
+    this.changeClientTwoLink = page.locator("#amend-client-2");
   }
 
-  public void clickChangeLink() {
-    changeLink.click();
+  public void clickChangeClientOneLink() {
+    changeClientOneLine.click();
+  }
+
+  public void clickChangeClientTwoLink() {
+    changeClientTwoLink.click();
   }
 }
