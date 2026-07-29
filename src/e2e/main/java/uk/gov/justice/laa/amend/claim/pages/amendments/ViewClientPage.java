e429dfd8770009f1d40d7b69a66ec845edb283c8
gabrielageorgieva <gabriela.georgieva@justice.gov.uk>
2026-07-29 16:20:15 +0100

    tidy up

--- src/e2e/main/java/uk/gov/justice/laa/amend/claim/pages/amendments/ViewClientPage.java
+++ src/e2e/main/java/uk/gov/justice/laa/amend/claim/pages/amendments/ViewClientPage.java
@@ -9,12 +9,16 @@ public class ViewClientPage extends BaseAmendmentPage {
 
   private final Locator changeClientOneLink;
   private final Locator changeClientTwoLink;
-  private final Locator continueToCheckAmendmentsLink;
+  private final Locator continueButton;
 
   public ViewClientPage(Page page) {
     super(page);
     this.changeClientOneLink = page.locator("#amend-client-1");
     this.changeClientTwoLink = page.locator("#amend-client-2");
-    this.continueToCheckAmendmentsLink = page.locator("#check");
+    this.continueButton = page.locator("#check");
+  }
+
+  public void clickContinue() {
+      continueButton.click();
   }
 }
