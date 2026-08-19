8d248af5d924c57db2157f1ad7bc188f8342215a
Jamie Briggs <jamie.briggs@justice.gov.uk>
2026-08-19 14:49:40 +0100

    BC-662 | Rename CrimeMinRepresentationOrderDateValidator

--- laa-amend-a-claim-ui/src/main/java/uk/gov/justice/laa/amend/claim/forms/amendments/validators/CrimeRepresentationOrderDateValidator.java
+++ laa-amend-a-claim-ui/src/main/java/uk/gov/justice/laa/amend/claim/forms/amendments/validators/CrimeRepresentationOrderDateValidator.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import uk.gov.justice.laa.amend.claim.forms.amendments.AmendmentForm;
 import uk.gov.justice.laa.amend.claim.models.ClaimDetails;
-import uk.gov.justice.laa.amend.claim.utils.DateWrapperUtil;
 import uk.gov.justice.laa.amend.claim.viewmodels.viewfield.ClaimViewField;
 import uk.gov.justice.laa.amend.claim.viewmodels.viewfield.CrimeClaimDetailsViewField;
 
@@ -16,12 +15,8 @@ public class CrimeRepresentationOrderDateValidator extends AmendmentDateValidato
 
   private static final LocalDate EARLIEST_MIN_REP_ORDER_DATE = LocalDate.of(2016, Month.APRIL, 1);
 
-  private final DateWrapperUtil dateWrapperUtil;
-
-  public CrimeRepresentationOrderDateValidator(
-      MessageSource messageSource, DateWrapperUtil dateWrapperUtil) {
+  public CrimeRepresentationOrderDateValidator(MessageSource messageSource) {
     super(messageSource);
-    this.dateWrapperUtil = dateWrapperUtil;
   }
 
   @Override
@@ -32,15 +27,11 @@ public class CrimeRepresentationOrderDateValidator extends AmendmentDateValidato
   @Override
   public void validate(
       ClaimDetails claim, ClaimViewField<?> field, AmendmentForm form, Errors errors) {
-    var representationOrderDate = form.getDateValue(field.name());
+    var representationOrderDate =
+        form.getDateValue(CrimeClaimDetailsViewField.REPRESENTATION_ORDER_DATE.toString());
 
-    if (representationOrderDate == null) {
-      return;
-    }
-
-    if (dateWrapperUtil.isFutureDate(representationOrderDate)) {
-      addDateInTheFutureMessage(errors, field);
-    } else if (representationOrderDate.isBefore(EARLIEST_MIN_REP_ORDER_DATE)) {
+    if (representationOrderDate != null
+        && representationOrderDate.isBefore(EARLIEST_MIN_REP_ORDER_DATE)) {
       addDateTooEarlyMessage(errors, field, EARLIEST_MIN_REP_ORDER_DATE);
     }
   }
