47c6cecb2f7223f36ab8ca9ca489dad59c0e80a8
paul.lynch <Paul.Lynch@justice.gov.uk>
2026-08-28 12:05:13 +0100

    BC-769: Mandatory field validation

--- laa-amend-a-claim-ui/src/main/java/uk/gov/justice/laa/payments/amend/forms/amendments/validators/rules/model/FieldConfig.java
+++ laa-amend-a-claim-ui/src/main/java/uk/gov/justice/laa/payments/amend/forms/amendments/validators/rules/model/FieldConfig.java
@@ -2,4 +2,4 @@ package uk.gov.justice.laa.payments.amend.forms.amendments.validators.rules.mode
 
 import java.util.List;
 
-public record FieldConfig(List<String> ruleGroups, List<RuleDto> rules) {}
+public record FieldConfig(List<String> ruleGroups) {}
