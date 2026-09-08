8f563d1bc225e7546b2b5fb91e61b4d13482276c
paul.lynch <Paul.Lynch@justice.gov.uk>
2026-09-08 14:54:29 +0100

    BC-769: Mandatory field validation

--- e2e/src/main/java/uk/gov/justice/laa/payments/amend/pages/amendments/AmendClient2Page.java
+++ e2e/src/main/java/uk/gov/justice/laa/payments/amend/pages/amendments/AmendClient2Page.java
@@ -5,6 +5,7 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.SelectOption;
 import uk.gov.justice.laa.payments.amend.pages.LaaPage;
 
 public class AmendClient2Page extends LaaPage {
@@ -18,7 +19,12 @@ public class AmendClient2Page extends LaaPage {
   }
 
   public void selectSelectionValue(String id, String value) {
-    page.locator(String.format("#%s", id)).selectOption(value);
+    var select = page.locator("#" + id);
+    if (value == null || value.isBlank()) {
+      return;
+    }
+    assertThat(select).isVisible();
+    select.selectOption(new SelectOption().setLabel(value));
   }
 
   public void fillInput(String inputKey, String value) {
